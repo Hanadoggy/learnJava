0faea373f4a7464e7f1bfc72989641283b3bf7aa
Hanadoggy <78478981+Hanadoggy@users.noreply.github.com>
2023-07-09 20:57:25 +0900

    add linked list and tree
    linked list, tree 구현 exercise

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,26 @@ import basic.Array;
 import basic.Autoboxing;
 import basic.MoreLists;
 import basic.MyIterator;
+import exercise.section11.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Autoboxing.run();
+        SearchTree list = new SearchTree(null);
+        list.traverse(list.getRoot());
+
+        String stringData = "Darwin Brisbane Perth Melbourne Canberra Adelaide Sydney Canberra";
+        String[] data = stringData.split(" ");
+        for (String s : data) {
+            list.addItem(new Node(s));
+        }
+
+        list.traverse(list.getRoot());
+//        list.removeItem(new Node("Sydney"));
+//        list.traverse(list.getRoot());
+//        list.removeItem(new Node("Perth"));
+//        list.traverse(list.getRoot());
     }
 
 }
---
+++ src/main/java/exercise/section11/NodeList.java
@@ -0,0 +1,12 @@
+package exercise.section11;
+
+/**
+ * interface는 자동으로 public, abstract 선언됨
+ * 접근 지정자는 하위 구현체에서 상속받아 구현할때 변경, 인터페이스에서는 오직 public
+ */
+public interface NodeList {
+    ListItem getRoot();
+    boolean addItem(ListItem item);
+    boolean removeItem(ListItem item);
+    void traverse(ListItem root);
+}
