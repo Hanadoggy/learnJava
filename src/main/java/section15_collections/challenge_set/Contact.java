6b645becb89b28505c4bad0e1ca2b18220926997
Bae IkHyun <78478981+Hanadoggy@users.noreply.github.com>
2023-08-03 21:03:33 +0900

    add parts of section 15
    섹션 15, set 부분 추가

--- src/main/java/section15_collections/challenge_set/Contact.java
+++ src/main/java/section15_collections/challenge_set/Contact.java
@@ -2,6 +2,7 @@ package section15_collections.challenge_set;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 public class Contact {
@@ -44,7 +45,7 @@ public class Contact {
         return "%s: %s %s".formatted(name, emails, phones);
     }
 
-    public Contact getData(Contact contact) {
+    public Contact mergeContactData(Contact contact) {
 
         Contact newContact = new Contact(name);
         newContact.emails = new HashSet<>(this.emails);
@@ -53,5 +54,40 @@ public class Contact {
         newContact.phones.addAll(contact.phones);
 
         return newContact;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Contact contact = (Contact) o;
+
+        return getName().equals(contact.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 * getName().hashCode();
+    }
+
+    public void addEmail(String companyName) {
+
+        String[] names = name.split(" ");
+        String email = "%c%s@%s.com".formatted(name.charAt(0), names[names.length - 1],
+                companyName.replaceAll(" ", "").toLowerCase());
+        if (!emails.add(email)) {
+            System.out.println(name + " already has email " + email);
+        } else {
+            System.out.println(name + " now has email " + email);
+        }
+    }
+
+    public void replaceEmailIfExists(String oldEmail, String newEmail) {
+
+        if (emails.contains(oldEmail)) {
+            emails.remove(oldEmail);
+            emails.add(newEmail);
+        }
     }
 }
--- src/main/java/section15_collections/challenge_set/ContactMain.java
+++ src/main/java/section15_collections/challenge_set/ContactMain.java
@@ -1,7 +1,9 @@
 package section15_collections.challenge_set;
 
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class ContactMain {
 
@@ -11,6 +13,18 @@ public class ContactMain {
         List<Contact> phones = ContactData.getData("phone");
         printData("Phone List", phones);
         printData("Email List", emails);
+
+        Set<Contact> emailContacts = new HashSet<>(emails);
+        Set<Contact> phoneContacts = new HashSet<>(phones);
+        printData("Phone Contacts", phoneContacts);
+        printData("Email Contacts", emailContacts);
+
+        int index = emails.indexOf(new Contact("Robin Hood"));
+        Contact robinHood = emails.get(index);
+        robinHood.addEmail("Sherwood Forest");
+        robinHood.addEmail("Sherwood Forest");
+        robinHood.replaceEmailIfExists("RHood@sherwoodforest.com", "RHood@sherwoodforest.org");
+        System.out.println(robinHood);
     }
 
     public static void printData(String header, Collection<Contact> contacts) {
