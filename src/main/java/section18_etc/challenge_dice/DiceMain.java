731448befa70d83fb124f93fc22f9aa9347107a9
Bae IkHyun <78478981+Hanadoggy@users.noreply.github.com>
2023-08-10 20:59:31 +0900

    add parts of section18
    섹션 18 일부 추가

--- src/main/java/section18_etc/challenge_dice/DiceMain.java
+++ src/main/java/section18_etc/challenge_dice/DiceMain.java
@@ -1,39 +1,63 @@
 package section18_etc.challenge_dice;
 
-import java.util.Arrays;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class DiceMain {
 
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final Random r = new Random();
+
     public static void main(String[] args) {
 
-        Random r = new Random();
-        Scanner scanner = new Scanner(System.in);
-        int[] dices = r.ints(5, 1, 7).toArray();
-        while (true) {
-            printDices(dices);
-            String input = scanner.next();
-            if ("ALL".contains(input)) {
-                dices = r.ints(5, 1, 7).toArray();
-            } else if (input.isEmpty() || input == "") {
+        List<Integer> dices = new ArrayList<>();
 
-            } else {
-                String[] s = input.split(" ");
-
-
-            }
-        }
-
-
+        do {
+            rollDice(dices);
+        } while (!pickLosers(dices));
+        System.out.println("Game over, Real game would score and continue");
     }
 
-    private static void printDices(int[] dices) {
-        System.out.println("You're dice are: " + Arrays.toString(dices));
+    private static void rollDice(List<Integer> dices) {
+
+        int randomCount = 5 - dices.size();
+        var newDice = r.ints(randomCount, 1, 7)
+                .sorted()
+                .boxed()
+                .toList();
+        dices.addAll(newDice);
+        System.out.println("You're dice are : " + dices);
+    }
+
+    private static boolean pickLosers(List<Integer> dices) {
+
         System.out.println("Press Enter to Score.");
         System.out.println("Type \"ALL\" to re-roll all the dice.");
         System.out.println("List numbers (separated by spaces) to re-roll selected dice.");
         System.out.print("--> ");
+        String input = scanner.nextLine();
+        if (input.isBlank()) {
+            return true;
+        }
+        try {
+            removeDice(dices, input.split(" "));
+        } catch (Exception e) {
+            e.printStackTrace(System.out);
+            System.out.println("Bad input, Try again");
+        }
+
+        return false;
+    }
+
+    private static void removeDice(List<Integer> dices, String[] selected) {
+
+        if (selected.length == 1 && selected[0].contains("ALL")) {
+            dices.clear();
+        } else {
+            for (String removed : selected) {
+                dices.remove(Integer.valueOf(removed));
+            }
+            System.out.println("Keeping " + dices);
+        }
     }
 
 }
